13ebb6e576dc462c23cf427b34201fdf3c4fe850
Mavlonbek860 <mavlonbekhudojberdiev28@gmail.com>
2024-10-17 19:13:31 +0500

    added new improvements

--- joining_thread.py
+++ joining_thread.py
@@ -1,8 +1,8 @@
 from threading import Thread
 from time import sleep
 
-def my_function_1(args):
-    for i in range(args):
+def my_function_1(arg):
+    for i in range(arg):
         print("Child thread-1 running", i)
         sleep(0.5)
 
